0b13ef7ce652c99bd9b5481dd5e2f5f9ad5a7c85
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2026-09-19 10:30:02 +0200

    Revert "Finish the GPU's work on paper before stopping the scheduler"
    This reverts commit 05d9e3b70695b26419e2ff213b7c170ab1d12cf7.

--- src/Cafe/OS/libs/TCL/TCL.h
+++ src/Cafe/OS/libs/TCL/TCL.h
@@ -21,7 +21,6 @@ namespace TCL
 	// called from Latte code
 	bool TCLGPUReadRBWord(uint32& cmdWord);
 	void TCLGPUNotifyNewRetirementTimestamp();
-	void TCLGPUDeclareEverythingRetired();
 
 	COSModule* GetModule();
 }
